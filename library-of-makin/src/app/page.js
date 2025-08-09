67161dcf5ddc496ef7123c85d12fa952bebc4441
JM <132931580+JXMakin@users.noreply.github.com>
2025-08-08 21:34:51 -0300

    added not much...

--- library-of-makin/src/app/page.js
+++ library-of-makin/src/app/page.js
@@ -1,12 +1,18 @@
 import "./Styles/main-style.css";
 import Image from "next/image";
 
+function Book(){
+  return (
+    <image
+      src="" alt="interactable book component"/>
+  );
+}
 
 export default function Home() {
   return (
     <div>
       <header>
-
+        <Header />
       </header>
       <main>
 
